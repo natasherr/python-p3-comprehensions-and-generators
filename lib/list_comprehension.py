a10d60e2ddac4168553830a3cf5437c44fa4f820
Ashley Natasha <ashleyotsiula@gmail.com>
2024-11-30 11:26:03 +0300

    list comprehensions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even = [x for x in num_list if x % 2 == 0]
+    return even
 
 def make_exclamation(sentence_list):
-    pass
+    string =  [y + "!" for y in sentence_list]
+    return string
